fffb14be79712fc39d401afacf0f4d4ba550c6f8
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-04-08 00:33:13 -0700

    Fix basic app.test.jsx test
    The basic test in app.test.jsx was failing due to it not having a
    store. The error the test was giving was complaining that it should
    be wrapped in a <Provider. I decided to humor it and also gave it
    a mocked store (which was also a necessary part of resolving the
    error).

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Routes from '../routes';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
 configure({ adapter: new Adapter() });
+const mockStore = configureStore([])
 
 describe('app', () => {
   it('renders without crashing', () => {
-    mount(<Routes />);
+    shallow(
+      <Provider store={mockStore()}>
+        <Routes />
+      </Provider>
+    );
   });
 });
