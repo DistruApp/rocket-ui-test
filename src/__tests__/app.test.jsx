e6ab238f2eef7a6459102d29521d0668174bfdf5
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-04-12 01:18:44 -0700

    Update app render test to care about a full mount
    When we previously updated the app.test.jsx render test, we updated
    it to use a shallow rendering. This actually didn't ensure the full
    rendering of the app without crashing. It only ensured that Routes
    was being instantiated without crashing. However, we want to test
    that all the sub components also don't crash. To do so we must use
    `mount` instead of `shallow`. Additionally we need to provide a
    proper store state because of this.

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import configureStore from 'redux-mock-store';
+import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import Routes from '../routes';
+import { initialState } from '../stores/LaunchCollectionReducer';
 
 configure({ adapter: new Adapter() });
-const mockStore = configureStore([])
+const mockStore = configureMockStore([])
+const appStore = mockStore({launchCollection: initialState})
 
 describe('app', () => {
   it('renders without crashing', () => {
-    shallow(
-      <Provider store={mockStore()}>
+    mount(
+      <Provider store={appStore}>
         <Routes />
       </Provider>
     );
