13e5621b2537483ed80aff887c31cf3d383808ff
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-04-12 22:32:15 -0700

    Convert Launches view into a function component
    I plan on using react hooks for the new functionality as I prefer
    that methodology for maintaining lifecycle changes / managing state.
    Most of the updates will be from the `Launches` view on down, so it
    felt right to convert it so my state management is consistent. I
    could update the provider store to use useContext instead of using
    redux, and I still might, but for the time being I've decided to
    leave it be and see how I after actually implementing what you've
    asked me to do.

--- src/views/Launches.jsx
+++ src/views/Launches.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ConnectedView from './ConnectedView';
-import {fetchLaunchesIfNeeded} from "../actions/Launches";
+import { fetchLaunchesIfNeeded } from "../actions/Launches";
 import launch from '../components/Launch';
 
-class LaunchesView extends Component {
-  componentDidMount() {
-    const { dispatch, launchCollection } = this.props;
-    fetchLaunchesIfNeeded({ dispatch, launchCollection });
-  }
+const launchesView = ({ dispatch, launchCollection }) => {
 
-  getContent() {
-    const { launchCollection } = this.props;
+  // This is essentially is the same as componentDidMount
+  // When the function component runs for the first time this
+  // will trigger. The second argument is tracked for the life
+  // of the functional component. The use effect only triggers
+  // when the second argument changes. Since we're passing a
+  // constant value, it'll only fire on the first run
+  useEffect(() => {
+    fetchLaunchesIfNeeded({ dispatch, launchCollection});
+  }, [])
 
+  const generateLaunchesList = () => {
     if (!launchCollection || launchCollection.fetching) {
       return <div> LOADING </div>;
     }
@@ -29,21 +33,18 @@ class LaunchesView extends Component {
         </li>
       )
     }
-
     return <ul>{launches}</ul>;
   }
 
-  render() {
-    return (
-      <div>
-        <h2> SpaceX launches </h2>
-        {this.getContent()}
-      </div>
-    );
-  }
-}
+  return(
+    <div>
+      <h2> SpaceX launches </h2>
+      { generateLaunchesList() }
+    </div>
+  );
+};
 
-LaunchesView.propTypes = {
+launchesView.propTypes = {
   dispatch: PropTypes.func.isRequired,
   /* eslint-disable react/forbid-prop-types */
   // TODO: It would be nice to be more specific and not disable this
@@ -51,4 +52,4 @@ LaunchesView.propTypes = {
   launchCollection: PropTypes.object.isRequired
 }
 
-export default ConnectedView(LaunchesView, 'launches');
+export default ConnectedView(launchesView, 'launches');
