ca665f607a75f442e1d28ac4e93ac6910a391a71
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-04-13 23:09:09 -0700

    Add tests for Rocket component

---
+++ src/__tests__/components/rocket.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import axios from 'axios';
+import Rocket from '../../components/Rocket';
+import { exampleRocket, exampleRocketId } from '../helpers';
+
+const runAllPromises = () => new Promise(setImmediate);
+jest.mock('axios');
+
+configure({ adapter: new Adapter() });
+
+describe('rocket', () => {
+  beforeEach(() => {
+    axios.mockClear();  
+  });
+  
+  it('renders when required props provided', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: exampleRocket,
+      status: 200
+    });
+    
+    const wrapper = mount(<Rocket {...{rocketId: exampleRocketId}} />);
+    await runAllPromises();
+    wrapper.update();
+    expect(wrapper.find('table')).toHaveLength(1);
+  });
+});
