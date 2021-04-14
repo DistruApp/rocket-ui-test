e8c01dc5455ef6b15ffe41b0f04fb0562bf60e4d
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-04-13 22:09:06 -0700

    Update Launch tests

--- src/__tests__/components/launch.test.jsx
+++ src/__tests__/components/launch.test.jsx
@@ -1,26 +1,45 @@
+import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import launch from '../../components/Launch'
-import { exampleLaunch } from '../helpers'
+import { cloneDeep } from 'lodash';
+import Launch from '../../components/Launch';
+import { exampleLaunch } from '../helpers';
 
 configure({ adapter: new Adapter() });
 
+const defaultProps = {
+  showRocket: false,
+  showHideThisRocket: () => null,
+  ...exampleLaunch
+}
+
 describe('launch', () => {
   it('renders when required props provided', () => {
-    shallow(
-      launch(exampleLaunch)
-    );
+    shallow(<Launch {...defaultProps} />);
   });
 
-  it('crashes when required props aren\'t provided', () => {
-    // Using enzyme's `expect(...).toThrow(...)` wasn't working so this
-    // is the round about way
-    try {
-      shallow(
-        launch()
-      );
-      throw new("Calling launch() with invalid props didn't crash");
-    }
-    catch(err) {} // eslint-disable-line no-empty
+  it('doesn\'t render a rocket when showRocket is false', () => {
+    const wrapper = shallow(<Launch {...defaultProps} />);
+    expect(wrapper.find("Rocket")).toHaveLength(0);
+  });
+
+  it('does render a rocket when showRocket is true', () => {
+    const props = cloneDeep(defaultProps)
+    props.showRocket = true;
+    
+    const wrapper = shallow(<Launch {...props} />);
+    expect(wrapper.find("Rocket")).toHaveLength(1);
+  });
+
+  it('triggers showHideThisRocket when button is clicked', () => {
+    const props = cloneDeep(defaultProps)
+    props.showHideThisRocket = jest.genMockFunction();
+
+    const wrapper = shallow(<Launch {...props} />);
+    const button = wrapper.find('button');
+    expect(button).toHaveLength(1);
+
+    button.simulate('click');
+    expect(props.showHideThisRocket.mock.calls.length).toEqual(1);
   });
 });
