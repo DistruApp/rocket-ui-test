86fa9e8369bcfff7021568bb9e94df3f691e2816
James Gentes <jgentes@users.noreply.github.com>
2021-05-01 07:45:43 -0700

    Replace webpack with Parcel (#1)
    * replaced webpack with parcel2
    
    * remove grunt

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
 import Routes from './routes'
 
-import "styles/base/_main.sass"  // Global styles
-import "styles/base/_common.sass"  // Global styles
-import "styles/_style.sass"  // Css-module styles
+import "../styles/base/_main.sass"  // Global styles
+import "../styles/base/_common.sass"  // Global styles
+import "../styles/_style.sass"  // Css-module styles
 
 import { Provider } from "react-redux";
 import store from "./stores/Root.js";
