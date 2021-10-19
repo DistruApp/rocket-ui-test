1bacdd5dd69342f73c87252b2da1a703214a6213
aemann2 <aemann2@gmail.com>
2021-10-18 17:16:22 -0700

    added useEffect to Launches.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './views/Launches';
 
 const Routes = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Launches}/>
-      <Route path="/Launches" component={Launches}/>
-    </div>
-  </Router>
+	<Router>
+		<div>
+			<Route exact path='/' component={Launches} />
+			<Route path='/Launches' component={Launches} />
+		</div>
+	</Router>
 );
 
 export default Routes;
--- src/views/Launches.js
+++ src/views/Launches.js
@@ -1,50 +1,48 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ConnectedView from './ConnectedView';
-import {fetchLaunchesIfNeeded} from "../actions/Launches";
+import { fetchLaunchesIfNeeded } from '../actions/Launches';
 import Launch from '../components/Launch';
 
-class LaunchesView extends Component {
-  componentDidMount() {
-    const { dispatch, launchesCollection } = this.props;
-    fetchLaunchesIfNeeded({ dispatch, launchesCollection });
-  }
+const LaunchesView = (props) => {
+	useEffect(() => {
+		const { dispatch, launchesCollection } = props;
+		fetchLaunchesIfNeeded({ dispatch, launchesCollection });
+	}, []);
 
-  getContent() {
-    const { launchCollection } = this.props;
+	const getContent = () => {
+		const { launchCollection } = props;
 
-    if (!launchCollection || launchCollection.fetching) {
-      return <div> LOADING </div>;
-    }
+		if (!launchCollection || launchCollection.fetching) {
+			return <div> LOADING </div>;
+		}
 
-    if (!launchCollection.launches.length) {
-      return <div> NO DATA </div>;
-    }
+		if (!launchCollection.launches.length) {
+			return <div> NO DATA </div>;
+		}
 
-    let launches = [];
+		const launches = [];
 
-    for (let i = 0; i < launchCollection.launches.length; i++) {
-      const launch = launchCollection.launches[i];
+		for (let i = 0; i < launchCollection.launches.length; i++) {
+			const launch = launchCollection.launches[i];
 
-      launches.push(
-        <Launch {...{
-          key: launch.launch_id,
-          launch
-        }} />
+			launches.push(
+				<Launch
+					{...{
+						key: launch.launch_id,
+						launch,
+					}}
+				/>
+			);
+		}
+		return <ul>{launches}</ul>;
+	};
 
-      )
-    }
-
-    return <ul>{launches}</ul>;
-  }
-
-  render() {
-    return (
-      <div>
-        <h2> SpaceX launches </h2>
-        {this.getContent()}
-      </div>
-    );
-  }
-}
+	return (
+		<div>
+			<h2> SpaceX launches </h2>
+			{getContent()}
+		</div>
+	);
+};
 
 export default ConnectedView(LaunchesView, 'launches');
